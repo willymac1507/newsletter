afaef32592b7751b3b1ba52cca88cbc58a06fb49
Will McCloy <william.mccloy@googlemail.com>
2021-07-28 14:13:35 +0100

    Add .gitignore and Procfile

--- app.js
+++ app.js
@@ -44,6 +44,6 @@ app.post("/failure.html", async(req,res) => {
     res.redirect("/");
 })
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("Server is running on port 3000");
 })
